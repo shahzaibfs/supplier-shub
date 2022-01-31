0ae19da0c30aa10f74bf31250f3d6407b08c4ff1
shahzaib <shahzaibalam231@gmail.com>
2022-02-01 02:27:03 +0500

    home-gallery feature added

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,6 +5,7 @@ import {
   AiOutlineSearch,
   AiFillCaretDown,
   AiOutlineMenu,
+  AiOutlineCaretRight,
 } from "react-icons/ai";
 
 import "./__home.css";
@@ -47,7 +48,7 @@ function Home() {
         /* <------------smallHeader container------------> */}
         <header className="container-fluid bg-primary d-none d-md-block ">
           <div
-            className="text-white body-2 text-weight-regular container-fluid container-xxl d-flex align-items-center justify-content-end w-100  p-0 mx-0"
+            className="text-white body-2 text-weight-regular container-fluid container-xxl d-flex align-items-center justify-content-end w-100  p-0 mx-0 mx-md-auto p-md-2"
             style={{ height: "42px" }}
           >
             Welcome<span className="text-weight-bold mx-md-1 ">~Guest |</span>
@@ -144,12 +145,10 @@ function Home() {
         /* <------------ Banner container------------> */
         /* <------------ Banner container------------> */}
         <div
-          className="container-fluid position-relative container-xxl bg-primary-light mt-2"
+          className="container-fluid position-relative container-xxl bg-primary-light mt-4"
           style={{ height: "450px" }}
         >
-          <div
-            className="banner__info  position-absolute bg-primary-light-700 p-4 d-flex flex-column justify-content-center "
-          >
+          <div className="banner__info  position-absolute bg-primary-light-700 p-4 d-flex flex-column justify-content-center ">
             <p className="body-1 text-muted text-weight-regular">
               New Luma Yoga Collection
             </p>
@@ -162,13 +161,78 @@ function Home() {
           </div>
         </div>
 
-          {/* <----------- gallery  container------------> */
+        {/* <----------- gallery  container------------> */
         /* <------------ gallery container------------> */
         /* <------------ gallery container------------> */
         /* <------------ gallery container------------> */}
-        
-
-
+        <div className="container-fluid container-xxl mx-auto p-0 mt-4 W-100">
+          <div className="home__gallery">
+            <div className="bg-primary-light p-4">
+              <div className="d-flex flex-column align-items-start p-4">
+                <h1 className="heading-2 text-primary text-weight-bold">
+                  20% OFF
+                </h1>
+                <p className="body-1 text-muted text-weight-regular">
+                  Luma pants when you shop today*
+                </p>
+                <p className="body-2 text-primary text-weight-regular text-decoration-underline">
+                  Shop Pants <AiOutlineCaretRight className="ms-1" />
+                </p>
+              </div>
+            </div>
+            <div className="bg-primary-light p-4">
+              <div className="d-flex flex-column align-items-start w-50  ms-auto p-4">
+                <h1 className="heading-2 text-primary-light-700 text-weight-regular ">
+                  Even more ways to mix and match
+                </h1>
+                <p className="body-1 text-muted text-weight-regular">
+                  Buy 3 Luma tees get a 4th free
+                </p>
+                <p className="body-2 text-primary text-weight-regular text-decoration-underline">
+                  Shop teees <AiOutlineCaretRight className="ms-1" />
+                </p>
+              </div>
+            </div>
+            <div className="bg-primary-light p-4">
+              <div className="d-flex flex-column align-items-start w-75 ms-auto p-4">
+                <h1 className="heading-2 text-primary-light-700 text-weight-regular">
+                  Take it from Erin
+                </h1>
+                <p className="body-1 text-muted text-weight-regular">
+                  Luma founder Erin Renny shares her favorites!
+                </p>
+                <p className="body-2 text-primary text-weight-regular text-decoration-underline">
+                  Shop Erin Recommends <AiOutlineCaretRight className="ms-1" />
+                </p>
+              </div>
+            </div>
+            <div className="bg-primary-light p-4">
+              <div className="d-flex flex-column align-items-start h-100 justify-content-center w-75 ms-auto p-4 bg-primary-light-700">
+                <h1 className="heading-2 text-primary-light-700 text-weight-regular">
+                Twice around, twice as nice
+                </h1>
+                <p className="body-1 text-muted text-weight-regular">
+                Find conscientious, comfy clothing in our eco-friendly collection                </p>
+                <p className="body-2 text-primary text-weight-regular text-decoration-underline">
+                Shop Eco-Friendly <AiOutlineCaretRight className="ms-1" />
+                </p>
+              </div>
+            </div>
+            <div className="bg-primary-light p-4">
+              <div className="d-flex flex-column align-items-start w-100 ms-auto p-4 bg-primary-light-700">
+                <h1 className="heading-2 text-primary-light-700 text-weight-regular">
+                  Science meets performance
+                </h1>
+                <p className="body-1 text-muted text-weight-regular">
+                  Wicking to raingear, Luma covers you
+                </p>
+                <p className="body-2 text-primary text-weight-regular text-decoration-underline">
+                  Shop Performance <AiOutlineCaretRight className="ms-1" />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </section>
   );

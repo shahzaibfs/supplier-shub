335f0ae98008d4c5c4679c2205f0b659e35e81e0
shahzaib <shahzaibalam231@gmail.com>
2022-02-03 13:42:11 +0500

    shipping checkout 1 added

---
+++ src/Pages/Checkout/index.js
@@ -0,0 +1,230 @@
+import React from "react";
+import { AiOutlineCheck } from "react-icons/ai";
+
+function Checkout() {
+  return (
+    <>
+      {/* @ compp */}
+      <header
+        className="d-flex my-2 align-items-center justify-content-center justify-content-md-start"
+        style={{ height: "70px" }}
+      >
+        <div
+          className="flex-item text-center text-primary body-2 text-weight-bold "
+          style={{ minWidth: "150px" }}
+        >
+          <div className="position-relative d-flex justify-content-center">
+            <div
+              className="position-absolute bg-primary"
+              style={{
+                width: "100%",
+                height: "5px",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%,-50%)",
+                zIndex: 1,
+              }}
+            ></div>
+            <div
+              className="rounded-circle  bg-white d-flex justify-content-center align-items-center border-2 border-primary-5 "
+              style={{ width: "35px", height: "35px", zIndex: 2 }}
+            >
+              <AiOutlineCheck className="body-1 text-weight-bold " />
+            </div>
+          </div>
+          Shipping
+        </div>
+        <div
+          className="flex-item text-center text-primary body-2 text-weight-bold "
+          style={{ minWidth: "150px" }}
+        >
+          <div className="position-relative d-flex justify-content-center">
+            <div
+              className="position-absolute bg-primary-light"
+              style={{
+                width: "100%",
+                height: "5px",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%,-50%)",
+                zIndex: 1,
+              }}
+            ></div>
+            <div
+              className="rounded-circle  bg-white d-flex justify-content-center align-items-center border-2 border-secondary-5 body-2 "
+              style={{ width: "35px", height: "35px", zIndex: 2 }}
+            >
+              2
+            </div>
+          </div>
+          Review and payments
+        </div>
+      </header>
+      {/* main comp */}
+      <main>
+        <header className="py-2 heading-2 text-primary-light-700 border-bottom-primary">
+          Shipping Address
+        </header>
+        <section className="row mx-0 justify-content-between align-items-start">
+          <div className="col-12 col-md-6 col-xl-7 px-0 flex-grow-1 me-2 ">
+            <form className="w-100 w-md-75 w-xl-50 my-2">
+              <div className="form-group mt-2 bg-none ">
+                <label className="body-2 text-weight-regular text-primary-light-700">
+                  First name
+                </label>
+                <input
+                  type="text"
+                  class="form-control body-2 text-weight-regular text-primary-light-700"
+                  placeholder="your name"
+                />
+              </div>
+              <div className="form-group mt-2 body-2 text-weight-regular text-primary-light-700">
+                <label className="body-2 text-weight-regular text-primary-light-700">
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  className="form-control body-2 text-weight-regular text-primary-light-700"
+                  placeholder="your sir name"
+                />
+              </div>
+              <div className="form-group mt-2 ">
+                <label className="body-2 text-weight-regular text-primary-light-700">
+                  Company | Market Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control body-2 text-weight-regular text-primary-light-700"
+                  placeholder="your company name "
+                />
+              </div>
+              <div className="form-group mt-2 ">
+                <label className="body-2 text-weight-regular text-primary-light-700">
+                  Shipping Address
+                </label>
+                <textarea
+                  type="text"
+                  className="form-control body-2 text-weight-regular text-primary-light-700"
+                  placeholder="enter complete shipping address"
+                  rows={4}
+                />
+              </div>
+              <div className="form-group mt-2 ">
+                <label className="body-2 text-weight-regular text-primary-light-700">
+                  Phone No
+                </label>
+                <input
+                  type="tel"
+                  className="form-control body-2 text-weight-regular text-primary-light-700"
+                  placeholder="cc-xxx-xxxxxxx"
+                />
+              </div>
+              <div className="form-group mt-2 ">
+                <label className="text-primary-light-700 text-weight-regular body-2 ">
+                  State
+                </label>
+                <select class="form-select  text-primary-light-700 body-2 ">
+                  <option value="1">Islamabad</option>
+                  <option value="2">Lahore</option>
+                  <option value="3">Karachi</option>
+                </select>
+              </div>
+              <footer
+                className="heading-2 text-primary-light-700 text-weight-regular d-flex align-items-center border-bottom-primary"
+                style={{ minHeight: "70px" }}
+              >
+                Shipping methods
+              </footer>
+              <div className="mt-2">
+                <div class="form-check  pb-1 ">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="exampleRadios"
+                    value="option1"
+                  />
+                  <label className="form-check-label body-2 text-primary-light">
+                    Cash on Delivery
+                  </label>
+                </div>
+                <div class="form-check pb-1 ">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="exampleRadios"
+                    value="option1"
+                  />
+                  <label className="form-check-label body-2 text-primary-light">
+                    Cash on Delivery
+                  </label>
+                </div>
+              </div>
+              <button className="btn btn-bg-primary text-white text-weight-regular my-2 ms-auto">
+                Next
+              </button>
+            </form>
+          </div>
+          <div
+            className="col-12 col-md-5 col-xl-3 px-2 mt-3 flex-grow-0 bg-primary-light overflow-auto rounded py-2">
+            {/* @comp */}
+            <header
+              className="heading-2  text-primary-light-700 text-weight-regular d-flex align-items-center"
+              style={{ height: "70px" }}
+            >
+              Order Summary
+            </header>
+            <p className="text-muted body-1">2 items in cart</p>
+            <article className="d-flex justify-content-between border-top-primary py-2 w-100">
+              <div className="d-flex">
+                <span className="me-2">
+                  {" "}
+                  <img
+                    src="https://image.shutterstock.com/image-photo/image-shocked-young-brunette-lady-260nw-641814046.jpg"
+                    alt=""
+                    height={70}
+                    width={70}
+                    style={{objectFit:"cover"}}
+                    className="bg-primary"
+                  />
+                </span>
+                <span>
+                    <h1 className="body-2  text-decoration-underline text-weight-regular">Go get Pushup Grips</h1>
+                    <p className="body-2   text-weight-regular">Qty : 1</p>
+                </span>
+              </div>
+              <price>
+                  $45.00
+              </price>
+
+            </article>
+            <article className="d-flex justify-content-between border-top-primary py-2 w-100">
+              <div className="d-flex">
+                <span className="me-2">
+                  {" "}
+                  <img
+                    src="https://image.shutterstock.com/image-photo/image-shocked-young-brunette-lady-260nw-641814046.jpg"
+                    alt=""
+                    height={70}
+                    width={70}
+                    style={{objectFit:"cover"}}
+                    className="bg-primary"
+                  />
+                </span>
+                <span>
+                    <h1 className="body-2  text-decoration-underline text-weight-regular">Go get Pushup Grips</h1>
+                    <p className="body-2   text-weight-regular">Qty : 1</p>
+                </span>
+              </div>
+              <price>
+                  $45.00
+              </price>
+
+            </article>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Checkout;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -9,7 +9,7 @@ const Signup = React.lazy(() => import("../Pages/Signup"));
 const Catalog = React.lazy(() => import("../Pages/Catalog"));
 const Product = React.lazy(() => import("../Pages/Product"));
 const Cart = React.lazy(() => import("../Pages/Cart"));
-
+const Checkout = React.lazy(() => import("../Pages/Checkout"));
 
 const Routes = () => {
   return (
@@ -24,7 +24,12 @@ const Routes = () => {
           <Route path="cart" element={<Cart />} />
 
           <Route path="products/:productCategory" element={<Catalog />} />
-          <Route path = "products/:productCategory/:id" element={<Product />} />
+          <Route path="products/:productCategory/:id" element={<Product />} />
+          {/* ----------Private Routes-----------  */
+          /* ----------Private Routes-----------  */
+          /* ----------Private Routes-----------  */
+          /* ----------Private Routes-----------  */}
+          <Route path="checkout" element={<Checkout />} />
         </Route>
         {/* ----------Auth Routing-----------  */
         /* ----------Auth Routing-----------  */
@@ -32,10 +37,6 @@ const Routes = () => {
         /* ----------Auth Routing-----------  */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* ----------Private Routes-----------  */
-        /* ----------Private Routes-----------  */
-        /* ----------Private Routes-----------  */
-        /* ----------Private Routes-----------  */}
       </Switch>
     </React.Suspense>
   );

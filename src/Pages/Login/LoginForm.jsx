dc04ae06793b16de2f836608e475783148f0b0a3
shahzaib <shahzaibalam231@gmail.com>
2022-03-02 20:51:10 +0500

    small changes to login and signup page

---
+++ src/Pages/Login/LoginForm.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
+const LoginForm = () => {
+  return (
+    <>
+      <section className="row mx-auto w-100 h-75 justify-content-center align-items-center flex-wrap">
+        <div className="col-10 col-md-6 col-lg-8 col-xxl-6 h-75 ">
+          {/* header for login form  */}
+          <div className="row mb-4  ">
+            <p className="text-primary-light-800 body-1 ">Welcome Back</p>
+            <h1 className="text-primary heading-2 text-wrap">
+              Login to your account
+            </h1>
+          </div>
+          {/* form starts from here  */}
+          <form className="mb-4">
+            <div class="form-group mb-4">
+              <label className="text-primary-light-700 body2">Email</label>
+              <input
+                type="email"
+                class="form-control"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+            </div>
+            <div class="form-group mb-4">
+              <label className="text-primary-light-700 body2">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                aria-describedby="passwordHelp"
+                placeholder="Your Password"
+              />
+            </div>
+            <div className="row m-0 justify-content-space-between mb-4">
+              <div class="form-group form-check col">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label
+                  class="form-check-label"
+                  for="exampleCheck1"
+                  className="body-2 text-primary-light-700"
+                >
+                  Remember me
+                </label>
+              </div>
+              <div class="col m-0 p-0">
+                <label
+                  for="exampleCheck1"
+                  className="body-2 text-secondary "
+                  style={{ float: "right" }}
+                >
+                  Forgot your password ?
+                </label>
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <button
+                type="button"
+                class="btn btn-bg-success text-white text-weight-regular body-1 w-100"
+              >
+                Login now
+              </button>
+            </div>
+            <div className="mb-5">
+              <button
+                type="button"
+                class="btn btn-bg-primary text-white text-weight-regular body-1 w-100 "
+              >
+                <FcGoogle /> <span className="mx-2">or signin with Google</span>
+              </button>
+            </div>
+          </form>
+          {/* fotter here  */}
+          <footer className="col-12 col-xxl-12  pb-5 pb-lg-0">
+            <p className="text-primary-light-800 body-2 text-center">
+              Don't have an account ?{" "}
+              <span className="text-secondary-light-800 body-2">
+                <Link to={"/signup"}>Signup for free</Link>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LoginForm;
---
+++ src/Pages/Signup/SignupForm.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
+const SignupForm = () => {
+  return (
+    <>
+      <section className="row mx-auto w-100 h-75 justify-content-center align-items-center flex-wrap">
+        <div className="col-10 col-md-6 col-lg-8 col-xxl-6 h-75 ">
+          {/* header signup page  */}
+          <div className="row mb-4  ">
+            <p className="text-primary-light-800 body-1 ">Welcome Guest</p>
+            <h1 className="text-primary heading-2 text-wrap">
+              Signup to your account
+            </h1>
+          </div>
+
+          {/* main form signup  */}
+          <form className="mb-4">
+            <div class="fullName form-group mb-4">
+              <label className="text-primary-light-700 body2">Full Name</label>
+              <input type="text" class="form-control" placeholder="Your Name" />
+            </div>
+            <div class="PhNo form-group mb-4">
+              <label className="text-primary-light-700 body2">Phone number</label>
+              <input
+                type="tel"
+                class="form-control"
+                placeholder="CC-XX-XXXXXXX"
+              />
+            </div>
+            <div class="Email form-group mb-4">
+              <label className="text-primary-light-700 body2">Email</label>
+              <input
+                type="email"
+                class="form-control"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+            </div>
+            <div class="Password form-group mb-4">
+              <label className="text-primary-light-700 body2">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                aria-describedby="passwordHelp"
+                placeholder="Your Password"
+              />
+            </div>
+            <div class="AccountType form-group mb-4">
+              <label className="text-primary-light-700 body2">
+                Account type
+              </label>
+              <select class="form-control">
+                <option>Supplier</option>
+                <option>Customer</option>
+              </select>
+            </div>
+
+            <div className="SignupButton  mb-3">
+              <button
+                type="button"
+                class="btn btn-bg-success text-white text-weight-regular body-1 w-100"
+              >
+                Signup
+              </button>
+            </div>
+            <div className="OrSignupWithGoogle mb-5">
+              <button
+                type="button"
+                class="btn btn-bg-primary text-white text-weight-regular body-1 w-100 "
+              >
+                <FcGoogle /> <span className="mx-2">or signin with Google</span>
+              </button>
+            </div>
+          </form>
+          {/* footer for signup  */}
+          <footer className="col-12 col-xxl-12  pb-5 pb-lg-2">
+            <p className="text-primary-light-800 body-2 text-center">
+              Have an account ?{" "}
+              <span className="text-secondary-light-800 body-2">
+               <Link to={"/login"}>Login Now!</Link> 
+              </span>
+            </p>
+          </footer>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SignupForm;

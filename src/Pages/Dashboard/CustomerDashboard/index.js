cb39b5ad6d715c8698ac7c30a4b4c8f38cfaf247
shahzaib <shahzaibalam231@gmail.com>
2022-03-12 12:08:39 +0500

    minor bug fixes customer dashborad

--- src/Pages/Dashboard/CustomerDashboard/index.js
+++ src/Pages/Dashboard/CustomerDashboard/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
+import ViewInventory from '../SupplierDashboard/Inventory/ViewInventory'
 import Index from "../SupplierDashboard/SupplierSideBar"
 
 const CustomerDashboard = () => {
   const user = useSelector(store=>store.authReducer) 
-  console.log(user)
+  const [Comp, setComp] = useState(null);
+  const setCompRef = (Comp) => {
+    setComp(Comp);
+  };
+  console.log(Comp)
   if(user.userDetails.accountType === "CUSTOMER"){
     return (
-      <div style={{height:"100vh"}}>
+      <div className='d-flex w-100' style={{height:"100vh", marginTop:"10px"}}>
 
-        <Index />
+        <Index ftnRef={setCompRef}/>
+        <section className="main h-100 w-75 " style={{background:"transparent"}}>
+          {Comp !== null ? Comp : <ViewInventory />}
+        </section>
 
       </div>
     )
